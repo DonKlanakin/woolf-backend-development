d55f04c8dea4aaaaff6592cc0e384f9bb13261ac
DK <don@promptnow.com>
2025-07-19 18:42:14 +0700

    Introduction to Express routing

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello DK!');
-})
+    res.status(200).send('[GET] Hello DK!');
+});
+
+app.post('/hello', (req, res) => {
+    res.status(200).send('[POST] Hello DK!');
+});
+
+app.post('/customers', (req, res) => {
+    res.status(200).json({
+        firstName: "Don",
+        lastName: "Klanakin"
+    });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
