0fcef8cee564bb91998f49ff4e610b997a392f9b
DK <don@promptnow.com>
2025-07-19 19:24:20 +0700

    GET Requests

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 const express = require('express');
+const fs = require('fs');
+const { cwd } = require('process');
+
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-    res.status(200).send('[GET] Hello DK!');
-});
+let data = JSON.parse(fs.readFileSync("./students.text"));
 
-app.post('/hello', (req, res) => {
-    res.status(200).send('[POST] Hello DK!');
-});
-
-app.post('/customers', (req, res) => {
-    res.status(200).json({
-        firstName: "Don",
-        lastName: "Klanakin"
-    });
+app.get('/api/v1/students', (req, res) => {
+    res.status(200).json({"status": "success", "data": data});
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
