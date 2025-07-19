69845fe6a54e9687160ab3bbc5b7e706d0684de9
DK <don@promptnow.com>
2025-07-20 00:33:15 +0700

    POST Requests

--- index.js
+++ index.js
@@ -5,10 +5,25 @@ const { cwd } = require('process');
 const app = express();
 const port = 8080;
 
+app.use(express.json());
+
 let data = JSON.parse(fs.readFileSync("./students.text"));
 
 app.get('/api/v1/students', (req, res) => {
     res.status(200).json({"status": "success", "data": data});
+});
+
+app.post('/api/v1/students', (req, res) => {
+    let body = req.body;
+    let response = data.filter((e) => e.Name.includes(body.Name));
+    console.log(response);
+    res.status(200).json({response});
+});
+
+app.post('/api/v1/students/:id', (req, res) => {
+    let id = req.params.id;
+    let response = data.find((e) => e.ID == id);
+    res.status(200).json({response});
 });
 
 app.listen(port, () => {
