3737f3f9a8c1095ba94b7751e5a8dc5824d4589b
DK <don@promptnow.com>
2025-07-27 12:25:51 +0700

    Update userService

--- services/userService.js
+++ services/userService.js
@@ -6,20 +6,20 @@ let data = JSON.parse(fs.readFileSync("./students.text"));
 exports.getAllUsers = async (req, res) => {
     let sql = 'SELECT NOW()'
     let response = await pool.query(sql);
-    console.log(response);
-    res.status(200).json({data});
+    let resDateTime = response.rows.at(0).now;
+    res.status(200).json({"status": "success", "requestSentAt": req.requestedAt, "responseReceivedAt": resDateTime, "data": data});
 }
 
 exports.getUsersByQueryParams = (req, res) => {
     let body = req.body;
     let response = data.filter((e) => e.Name.includes(body.Name));
-    res.status(200).json({"status": "success", "requestdAt": req.requestedAt, "data": response});
+    res.status(200).json({"status": "success", "requestSentAt": req.requestedAt, "data": response});
 }
 
 exports.getUserById = (req, res) => {
     let params = req.params;
     let response = data.find((e) => e.ID == params.id);
-    res.status(200).json({"status": "success", "requestdAt": req.requestedAt, "data": response});
+    res.status(200).json({"status": "success", "requestSentAt": req.requestedAt, "data": response});
 }
 
 exports.updateUserById = (req, res) => {
